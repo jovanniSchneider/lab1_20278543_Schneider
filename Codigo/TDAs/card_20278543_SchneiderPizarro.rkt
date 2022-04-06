b38db1bf94e34e187174b3da91ca2190b6595ebb
Jovanni Schneider <jovanni.schneider@usach.cl>
2022-04-06 00:46:09 -0400

    V 0.0.3
    Se corrigen errores en los TDAs

--- Codigo/TDAs/card_20278543_SchneiderPizarro.rkt
+++ Codigo/TDAs/card_20278543_SchneiderPizarro.rkt
@@ -9,9 +9,8 @@
 ;Recorrido: list
 ;Funcion: Crea una entidad de card en caso de que los parametros ingresados correspondan al dominio del TDA
 (define card (lambda (cardinal . figuras)
-    (define preCard (lazy (list cardinal figuras)))
-        (if (card? (force preCard))
-            (force preCard)
+        (if (and (number? cardinal) (list? figuras) (= cardinal (length figuras)))
+            (list cardinal figuras)
             null
         )        
     )
@@ -22,8 +21,11 @@
 ;Recorrido: Bool
 ;Funcion: Verifica si una lista corresponde al TDA card, devuelve true o false
 (define card? (lambda (lista)
-        (if (and (number? (car lista)) (list? (cdr lista)))
-            (= (car lista) (length (cadr lista)))
+        (if (and (list? lista) (not (null? lista)))
+            (if (and (number? (car lista)) (list? (cdr lista)) (= 2 (length lista)))
+                (= (car lista) (length (cadr lista)))
+                #f
+            )
             #f
         )
     )
@@ -31,9 +33,46 @@
 
 ;------------------------------Selectores------------------------------
 ;Entrega el numero de simbolos que contiene la carta
-(define card->getCardinal car)
+(define card->getCardinal (lambda (lista)
+        (if (card? lista)
+            (car lista)
+            null
+        )
+    )
+)
 ;Entrega la lista de simbolos de la carta
-(define card->getSimbolos cadr)
+(define card->getSimbolos (lambda (lista)
+        (if (card? lista)
+            (cadr lista)
+            null
+        )
+    )
+)
+
+;--------------------------Funciones propias---------------------------
+
+;Dominio: card
+;Recorrido: Bool
+;Funcion: Verifica si una carta solo tiene simbolos distintos
+
+(define card->JustDifferentSymbols (lambda (carta)
+        (if (list? (card->getSimbolos carta))
+            (= (length (card->getSimbolos carta)) (length (filter (card->just1Time (card->getSimbolos carta)) (card->getSimbolos carta))))
+            #f
+        )
+    )
+)
+
+;Dominio: card
+;Recorrido: Bool
+;Funcion: Verifica que un simbolo solo aparezca una vez en una lista
+(define card->just1Time (lambda (listaDeSimbolos) 
+            (lambda (simbolo)
+                (= 1 (length (filter (lambda (simb)(equal? simb simbolo))listaDeSimbolos))) 
+        )
+    )
+)
 
 
-(define e1 (card 3 "pepe" "juan" "s"))
+;Ejemplo de uso
+(define e1 (card 3 1 2 2))
